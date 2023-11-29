97bad15eb4251fab477c123c1e88dae18266db30
TzvetomirTz <cvetomiriv.cvetkov@gmail.com>
2023-11-30 01:52:18 +0200

    Specify region in the serverless.yml

--- lambdas/generateBitPazaarAccessKey.js
+++ lambdas/generateBitPazaarAccessKey.js
@@ -24,10 +24,8 @@ module.exports.handler = async (event) => {
 
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
+    body: {
         message: 'Hi!'
-      }
-    ),
+    },
   };
 };

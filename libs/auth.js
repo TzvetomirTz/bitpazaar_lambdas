cfc89d20d7270867b351ae6cb1afc60b6279171c
TzvetomirTz <cvetomiriv.cvetkov@gmail.com>
2023-12-15 11:28:01 +0200

    Fix tests to use mocked Date.getTime()

--- libs/auth.js
+++ libs/auth.js
@@ -5,7 +5,7 @@ const verifyAndDecodeJwt = (token) => {
     let cert = fs.readFileSync('./resources/auth_public_key');
     let decodedToken;
 
-	jwt.verify(shid, cert, function (err, decoded) {
+	jwt.verify(token, cert, function (err, decoded) {
 		if (err) {
 			console.error("Error", err);
 		}
--- test/generateBitPazaarAccessKeyTests.js
+++ test/generateBitPazaarAccessKeyTests.js
@@ -1,24 +1,31 @@
 const assert = require('assert');
-const { verify } = require('crypto');
+const sinon = require("sinon");
+const jwt = require('jsonwebtoken');
 const generateBitPazaarAccessKey = require('../lambdas/generateBitPazaarAccessKey').handler;
+const verifyAndDecodeJwt = require('../libs/auth').verifyAndDecodeJwt;
 
 describe('Base Gen BitP Acs Key', () => {
+    
+    sinon.stub(Date.prototype, 'getTime').returns(1702602445794);
+
     describe('Gen', () => {
         it('asdf', async () => {
             const walletAddress = "0x860eF1218860bf039F4962aa9B15307E8fb16087";
 
             const event = {
                 queryStringParameters: {
-                    walletAddress,
+                    wltAddr: walletAddress,
                     action: 'auth',
-                    block: 1,
-                    signature: "0x1e4e1528b2a6a8ff7d4f078114c8e199858e19edd8717c11a2921eb467f6c3a67bce63f848f8378002ef2afae79a57acc41ccd179409cf8ff1bacf19ccc005711b"
+                    ogTs: 1702602445793,
+                    signature: "0xe43cbb3620d609f4ee062397c20937d80ec1f66ae9fedcaee33411ddb86934414a64c960b874d1fbf734505ed1d58ebad51d18392890daa8985aeaa1c83576b51c"
                 }
             };
 
             const resStr = await generateBitPazaarAccessKey(event);
             const resBody = JSON.parse(resStr.body);
-            assert.equal(walletAddress, resBody.recoveredAddress);
+            const decodedJwt = jwt.decode(resBody.acsToken);
+
+            assert.equal(walletAddress, decodedJwt.addr);
         });
     });
 });

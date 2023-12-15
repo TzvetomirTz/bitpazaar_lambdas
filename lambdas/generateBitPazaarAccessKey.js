a148d8b143c559d633a1d2f3e2790ac7eb87c674
TzvetomirTz <cvetomiriv.cvetkov@gmail.com>
2023-12-15 03:02:11 +0200

    Add ts checks and ttl to the jwt token, tests are to be fixed

--- lambdas/generateBitPazaarAccessKey.js
+++ lambdas/generateBitPazaarAccessKey.js
@@ -3,40 +3,50 @@
 const { ethers } = require("ethers");
 const AWS = require('aws-sdk');
 const base64url = require('base64url');
+const crypto = require("crypto");
 
 AWS.config.update({ region: 'eu-west-2' });
 
-const AWS_KMS_KEY_ID = "aaef6297-2af5-47f8-a0f6-341f8446e085";
+const AWS_KMS_KEY_ID = "aaef6297-2af5-47f8-a0f6-341f8446e085"; // ToDo: move to variable
+// MAX_REQUEST_AGE_MS has to be bigger in order to give the user enough time to sign
+const MAX_REQUEST_AGE_MS = 30_000; // ToDo: move to variable
+const ACS_TOKEN_TTL = 86_400_000; // 1 day; ToDo: move to variable
 
 const domain = {
 	name: 'BitPazaar',
-	chainId: 1
+	chainId: 1 // ToDo: move to variable
 };
 
 const types = {
 	Auth: [
-		{ name: 'walletAddress', type: 'address' },
+		{ name: 'wltAddr', type: 'address' },
 		{ name: 'action', type: 'string' },
-		{ name: 'block', type: 'uint256'}
+		{ name: 'ogTs', type: 'uint256'}
 	]
 };
 
 module.exports.handler = async (event) => {
 
-	// Verify auth payload was really signed by the said address
+	// Verify auth payload has benign data
 	const eventParams = event.queryStringParameters;
 	const kms = new AWS.KMS();
 
 	const authPayload = {
-		walletAddress: eventParams.walletAddress,
+		wltAddr: eventParams.wltAddr,
 		action: 'auth',
-		block: eventParams.block
+		ogTs: eventParams.ogTs
 	};
 
     const recoveredAddress = ethers.verifyTypedData(domain, types, authPayload, eventParams.signature);
 
-	if(recoveredAddress !== eventParams.walletAddress) {
-		throw new Error("Recovered address didn't match signer address."); // ToDo: return code 400 with message or whatevs
+	if(recoveredAddress !== eventParams.wltAddr) {
+		throw new Error("Recovered address didn't match signer address."); // ToDo: return code 4XX with message
+	}
+
+	const currTs = new Date().getTime();
+
+	if(eventParams.ogTs > currTs || currTs - eventParams.ogTs > MAX_REQUEST_AGE_MS) {
+		throw new Error(`Timestamp ${eventParams.ogTs} sent with auth request is invalid. Current: ${currTs}`); // ToDo: return code 4XX with message
 	}
 
 	// Issue JWT token
@@ -47,8 +57,9 @@ module.exports.handler = async (event) => {
 	};
 	
 	let payload = {
-		address: recoveredAddress,
-		validBefore: 1
+		addr: recoveredAddress,
+		endDate: currTs + ACS_TOKEN_TTL,
+		tokenId: crypto.randomUUID()
 	};
 
 	let token_components = {
@@ -71,7 +82,6 @@ module.exports.handler = async (event) => {
 	return {
 		"isBase64Encoded": false,
 		"statusCode": 200,
-		"headers": {},
-		"body": JSON.stringify({recoveredAddress, token}),
+		"body": JSON.stringify({acsToken: token}),
 	};
 };
